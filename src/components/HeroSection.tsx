798a5c7b3dfdc69eff018ce4a16ed48966d4a411
Ana Fariya <anafariya@gmail.com>
2024-02-29 23:00:47 +0530

    removed apostrophes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { Button } from "./ui/moving-border";
+
 function HeroSection() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
@@ -10,7 +11,7 @@ function HeroSection() {
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-        <h1 className="mt-24 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Welcome to Ana's Fitness Haven: Transform Your Life Today!</h1>
+        <h1 className="mt-24 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Welcome to Ana&apos;s Fitness Haven: Transform Your Life Today!</h1>
         <p className="mt-7 font-normal text-xl md:text-2xl text-neutral-300 max-w-lg mx-auto">
           Your Pathway to Fitness Success Starts Here. We Empower You to Reach
           Your Peak Fitness Potential!
--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -1,59 +1,58 @@
 "use client"
 import { InfiniteMovingCards } from "../components/ui/infinite-moving-cards";
-
 const gymTestimonialMessages = [
-    {
-      "quote": "I've never felt stronger since starting Strength Training with Emily Johnson. Her expertise and motivation keep me coming back for more!",
-      "name": "Michael Smith",
-      "title": "Strength Training Enthusiast"
-    },
-    {
-      "quote": "Cardio Blast with John Smith is an absolute game-changer! I've never felt more energized and alive after a workout.",
-      "name": "Emily Brown",
-      "title": "Cardio Blast Devotee"
-    },
-    {
-      "quote": "Sarah Thompson's Yoga Flow is my weekly dose of serenity. It's amazing how much better I feel after each session!",
-      "name": "Olivia Johnson",
-      "title": "Yoga Flow Lover"
-    },
-    {
-      "quote": "I never knew I could lift so much until I joined Powerlifting with Michael Davis. His encouragement pushes me to new heights!",
-      "name": "David Wilson",
-      "title": "Powerlifting Aficionado"
-    },
-    {
-      "quote": "Jessica Roberts's Pilates Fusion classes have transformed my posture and flexibility. I can't thank her enough!",
-      "name": "Sophia Miller",
-      "title": "Pilates Fusion Fanatic"
-    },
-    {
-      "quote": "Strength Training with Emily Johnson has helped me overcome my fitness plateau. Her dedication to her craft is truly inspiring!",
-      "name": "Lucas Harris",
-      "title": "Strength Training Enthusiast"
-    },
-    {
-      "quote": "I've never been more flexible and centered since joining Sarah Thompson's Yoga Flow class. It's the highlight of my week!",
-      "name": "Ava Taylor",
-      "title": "Yoga Flow Lover"
-    },
-    {
-      "quote": "Cardio Blast with John Smith is the best way to kickstart my day. I'm addicted to the rush of endorphins I get after each session!",
-      "name": "Ethan Johnson",
-      "title": "Cardio Blast Devotee"
-    },
-    {
-      "quote": "Michael Davis's Powerlifting class has given me newfound strength and confidence. I never thought I could deadlift so much!",
-      "name": "Liam Brown",
-      "title": "Powerlifting Aficionado"
-    },
-    {
-      "quote": "Jessica Roberts's Pilates Fusion classes are like therapy for my body and mind. I leave feeling refreshed and invigorated every time!",
-      "name": "Isabella Martinez",
-      "title": "Pilates Fusion Fanatic"
-    }
-  ]
-  
+  {
+    "quote": "I&apos;ve never felt stronger since starting Strength Training with Emily Johnson. Her expertise and motivation keep me coming back for more!",
+    "name": "Michael Smith",
+    "title": "Strength Training Enthusiast"
+  },
+  {
+    "quote": "Cardio Blast with John Smith is an absolute game-changer! I&apos;ve never felt more energized and alive after a workout.",
+    "name": "Emily Brown",
+    "title": "Cardio Blast Devotee"
+  },
+  {
+    "quote": "Sarah Thompson&apos;s Yoga Flow is my weekly dose of serenity. It&apos;s amazing how much better I feel after each session!",
+    "name": "Olivia Johnson",
+    "title": "Yoga Flow Lover"
+  },
+  {
+    "quote": "I never knew I could lift so much until I joined Powerlifting with Michael Davis. His encouragement pushes me to new heights!",
+    "name": "David Wilson",
+    "title": "Powerlifting Aficionado"
+  },
+  {
+    "quote": "Jessica Roberts&apos;s Pilates Fusion classes have transformed my posture and flexibility. I can&apos;t thank her enough!",
+    "name": "Sophia Miller",
+    "title": "Pilates Fusion Fanatic"
+  },
+  {
+    "quote": "Strength Training with Emily Johnson has helped me overcome my fitness plateau. Her dedication to her craft is truly inspiring!",
+    "name": "Lucas Harris",
+    "title": "Strength Training Enthusiast"
+  },
+  {
+    "quote": "I&apos;ve never been more flexible and centered since joining Sarah Thompson&apos;s Yoga Flow class. It&apos;s the highlight of my week!",
+    "name": "Ava Taylor",
+    "title": "Yoga Flow Lover"
+  },
+  {
+    "quote": "Cardio Blast with John Smith is the best way to kickstart my day. I&apos;m addicted to the rush of endorphins I get after each session!",
+    "name": "Ethan Johnson",
+    "title": "Cardio Blast Devotee"
+  },
+  {
+    "quote": "Michael Davis&apos;s Powerlifting class has given me newfound strength and confidence. I never thought I could deadlift so much!",
+    "name": "Liam Brown",
+    "title": "Powerlifting Aficionado"
+  },
+  {
+    "quote": "Jessica Roberts&apos;s Pilates Fusion classes are like therapy for my body and mind. I leave feeling refreshed and invigorated every time!",
+    "name": "Isabella Martinez",
+    "title": "Pilates Fusion Fanatic"
+  }
+];
+
 
 function GymTestimonials() {
   return (
